3d86ef36cb18576df83159d717407261a48bfc50
jshepp27 <j.sheppard.ldn@gmail.com>
2021-10-20 20:15:05 +0100

    config

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-class DevelopmentConfig:
-    SECRET_KEY = os.getenv("SECRET_KEY", "default-key")
-    DATABASE_URI = os.environ['DATABASE_URI']
+# class DevelopmentConfig:
+#     SECRET_KEY = os.getenv("SECRET_KEY", "default-key")
+#     DATABASE_URI = os.environ['DATABASE_URI']
 
-class ProductionConfig(DevelopmentConfig):
+class ProductionConfig():
     PROD_URI = "postgres://huylpljjnoshbe:b51e90b80991f8e32ed4399a04c6db2a837f803995392e79472d1b92110ac60b@ec2-34-250-16-127.eu-west-1.compute.amazonaws.com:5432/dc7g7o62up02ai"
 
 # class StagingConfig(Config):
